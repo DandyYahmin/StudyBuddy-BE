954797b5e4240a12aceee958222477e5e6efc17c
DandyYahmin <dandydarmawanalyahmin@gmail.com>
2024-06-08 19:51:56 +0700

    cors setting

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 import router from './routes/index.js';
 import dotenv from 'dotenv';
 dotenv.config();
+import cors from 'cors';
 
 app.use(express.json());
 app.use(router);
+app.use(cors({
+    credentials: true,
+    origin: '*',
+    allowedHeaders: ['Content-Type']
+}));
 
 const port = process.env.SERVER_PORT;
 app.listen(port, () => console.log('listening on port ' + port));
