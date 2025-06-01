d46df70e92ab1c90f6733c177549d7325ff43f8a
DandyYahmin <dandydarmawanalyahmin@gmail.com>
2025-06-01 20:40:32 +0700

    fix: correct import path for TokenValidator in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { Login, LoginValidator, Logout } from '../controllers/AuthController.js';
 // import { MandatoryValidator, Mandatory } from '../middleware/Validator.js';
-import { TokenValidator, Token } from '../middleware/verifyToken.js';
+import { TokenValidator, Token } from '../middleware/VerifyToken.js';
 // import Log from '../middleware/Log.js';
 import pkg from 'express-group-routes';
 import { RegisterValidator, Register } from '../controllers/RegisterController.js';
